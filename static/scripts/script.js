fd03c00ca8e1da28da822c207234c132aa178314
gilos123 <61705439+gilos123@users.noreply.github.com>
2024-04-29 17:17:31 +0300

    Delete static/scripts/script.js
    deleted file

--- static/scripts/script.js
+++
@@ -1,6 +0,0 @@
-// document.querySelectorAll('a').forEach(tag => {
-//     tag.addEventListener('click', function() {
-//         document.querySelectorAll('a').forEach(t => t.classList.remove('active'));
-//         this.classList.add('active');
-//     });
-// });
